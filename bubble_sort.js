0bd00aae8dc99f73b888838dc4c9d27fb22166ca
alfandash <cumiasem91@gmail.com>
2017-07-26 17:57:29 +0700

    Release 1: sort using Bubble and selection Done

--- bubble_sort.js
+++ bubble_sort.js
@@ -1,14 +1,30 @@
 'use strict'
 
-function bubbleSort(items) {}
+function swapHelper(array,i,j) {
+  var temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
+}
+
+function bubbleSort(items) {
+  for (var i = 0; i < items.length; i++) {
+    for (var j = 1; j < items.length; j++) {
+      if(items[j-1] > items[j]){
+        swapHelper(items, j-1,j)
+      }
+    }
+  }
+  return items
+}
 
 
 // Driver code
 function main() {
 
-  sort_from_file('random_wordlist.txt')
-  sort_from_file('reversed_wordlist.txt')
-  sort_from_file('sorted_wordlist.txt')
+  sort_from_file('test.txt')
+  // sort_from_file('random_wordlist.txt')
+  // sort_from_file('reversed_wordlist.txt')
+  // sort_from_file('sorted_wordlist.txt')
 
 }
 
@@ -25,7 +41,7 @@ function sort_from_file(filename) {
 
     console.log(filename)
     console.log("--------")
-    console.log(bubbleSort2(items))
+    console.log(bubbleSort(items))
 
   })
 }
--- selection_sort.js
+++ selection_sort.js
@@ -1,14 +1,34 @@
 'use strict'
 
+function swapHelper(array,i,j) {
+  var temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
+}
+
 function selectionSort(arrays){
+  var temp = '';
+  for (var i = 0; i < arrays.length; i++) {
+    var min = i;
+    for (var j = i+1; j < arrays.length; j++) {
+      if (arrays[j] < arrays[min]) {
+        min = j
+      }
+    }
+    if (i !== min) {
+      swapHelper(arrays, i, min)
+    }
+  }
+  return arrays
 }
 
 // Driver code
 function main() {
 
-  sort_from_file('random_wordlist.txt')
-  sort_from_file('reversed_wordlist.txt')
-  sort_from_file('sorted_wordlist.txt')
+  sort_from_file('test.txt')
+  //sort_from_file('random_wordlist.txt')
+  //sort_from_file('reversed_wordlist.txt')
+  //sort_from_file('sorted_wordlist.txt')
 
 }
 
@@ -21,7 +41,9 @@ function sort_from_file(filename) {
       return console.log(err)
 
     let items = data.toString().split("\r\n")
-
+    //var items = [2,3,4,1,21,4,5,6,3]
+    selectionSort(items);
+    //console.log(items);
     console.log(filename)
     console.log("--------")
     console.log(selectionSort(items))
